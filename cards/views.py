29140b856638877316b9ae59e58a9f38c0579ddd
Kendyr Quintanilla <kendyr7@gmail.com>
2023-10-04 23:42:17 -0600

    Step 1: Prepare Django Project, Step 2: Set Up Flashcards App

---
+++ cards/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import (
+    ListView,
+)
+
+from .models import Card
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by("box", "-date_created")
